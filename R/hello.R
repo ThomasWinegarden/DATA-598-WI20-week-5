cffbc42f1cfe8172b2749f34f01b1b455271179b
Thomas Winegarden <Thomas@MacBook-Pro-3.local>
2020-02-10 22:42:55 -0800

    added rmd and r script in vignettes

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,20 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+
+#NOTES FROM SLIDES
+
+# usethis::use_mit_license("Ben Marwick")
+
+# usethis::use_package("dplyr", type = "Imports", min_version = "0.8.4")
+
+# usethis::use_vignette("name-of-my-vignette-file")
+
+# usethis::use_readme_md()
+
+# usethis::use_code_of_conduct()
+
+
+# knitr::read_chunk()
